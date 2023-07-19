ae614029d95d8d34d97226142ee076aec1d307c7
Alano Teles <alanoteles@gmail.com>
2023-07-19 17:50:48 -0300

    Values injection.

--- src/main/java/com/algaworks/algafoodapi/di/notification/MailNotificator.java
+++ src/main/java/com/algaworks/algafoodapi/di/notification/MailNotificator.java
@@ -2,6 +2,7 @@ package com.algaworks.algafoodapi.di.notification;
 
 import com.algaworks.algafoodapi.di.model.Client;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -11,13 +12,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class MailNotificator implements Notificator {
 
-    public MailNotificator(){
+    @Value("${mail.notificator.server-host}")
+    private String host;
 
-        System.out.println("MailNotificator PROD");
-    }
+    @Value("${mail.notificator.server-port}")
+    private Integer port;
 
     @Override
     public void notificate(Client client, String msg) {
+        System.out.println("Host:" + host);
+        System.out.println("Port:" + port);
 
         System.out.printf("Notifying %s using email %s: %s\n",
                 client.getName(), client.getEmail(), msg);
